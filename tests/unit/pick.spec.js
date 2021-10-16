55f6d3d5284dbca22b9253e864923f0a9fbacc1e
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-10-17 00:53:56 +0200

    tests: update test for randomness only for getRandomValue() and its min and max

--- tests/unit/pick.spec.js
+++ tests/unit/pick.spec.js
@@ -56,21 +56,23 @@ test("logger method log() is disabled when testing", () => {
 //TODO: A few testing parts coming soon
 
 //Tests for pick one member feature
-test("real randomness of getOneRandomMember without priority", () => {
+test("real randomness of getRandomValue and respect min and max", () => {
   const wrapper = mount(Pick);
 
   wrapper.vm.priorityMode = false;
   //Init variables
-  var result = {};
+  var result = [];
   var randomMemberId = null;
-  var nbAttempts = 100;
+  var nbAttempts = 10000;
   var biggestDifference = 0; //biggest difference of draws (between min and max draw counters)
   var max = 0;
   var min = nbAttempts;
+  var length = null;
+  var niceResult = "";
 
   //Launch getOneRandomMember() a big amount of times to have a sample of results
   for (var i = 0; i <= nbAttempts; i++) {
-    randomMemberId = wrapper.vm.getOneRandomMember(); //launch the generation
+    randomMemberId = wrapper.vm.getRandomValue(1, 10); //launch the generation
 
     //Save the result
     if (result[randomMemberId] == undefined) {
@@ -81,17 +83,42 @@ test("real randomness of getOneRandomMember without priority", () => {
 
   //Calculate the biggestDifference
   for (var possibility in result) {
+    length = result[possibility].length;
     //Search for the max
-    if (max < possibility.length) {
-      max = possibility.length;
+
+    if (max < length) {
+      max = length;
     }
     //Search for the min
-    if (min > possibility.length) {
-      min = possibility.length;
+    if (min > length) {
+      min = length;
     }
   }
   biggestDifference = max - min;
 
-  //Expect than the biggest difference is <= than 10% than nb of attempts
-  expect(biggestDifference).toBeLessThanOrEqual(nbAttempts * 0.1);
+  //Display a nice result:
+
+  niceResult =
+    "\n\nOn " +
+    nbAttempts +
+    " tries, here is the repartition of the draws (value and number of draws for this value):";
+  var minimumValueGenerated = null;
+  var maximumValueGenerated = null;
+  for (var item in result) {
+    if (minimumValueGenerated == null) {
+      minimumValueGenerated = item;
+    }
+    length = result[item].length;
+    niceResult += "\n  - " + item + ": " + length;
+    maximumValueGenerated = item;
+  }
+  console.log(niceResult);
+  console.log(min);
+  console.log(max);
+  console.log(biggestDifference);
+
+  expect(parseInt(minimumValueGenerated)).toBeGreaterThanOrEqual(0);
+  expect(parseInt(maximumValueGenerated)).toBeLessThanOrEqual(10);
+  //Expect than the biggest difference is <= than 1% than nb of attempts
+  expect(biggestDifference).toBeLessThanOrEqual(nbAttempts * 0.01);
 });
