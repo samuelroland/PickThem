4c414bc5d692876e4c9a4815caafc511a677162b
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2021-10-16 17:49:08 +0200

    Pick: test 2 basic texts

--- tests/unit/pick.spec.js
+++ tests/unit/pick.spec.js
@@ -1,3 +1,21 @@
 import { mount } from "@vue/test-utils";
 import Pick from "@/components/Pick.vue";
 
+test("App name is visible", () => {
+  //Arrange
+  const wrapper = mount(Pick);
+
+  //Assert
+  expect(wrapper.text()).toContain("PickThem");
+});
+
+test("Basic information are visible", () => {
+  //Arrange
+  const wrapper = mount(Pick);
+
+  //Assert
+  expect(wrapper.text()).toContain("GNU AGPLv3");
+  expect(wrapper.text()).toContain("licence libre");
+  expect(wrapper.text()).toContain("Samuel Roland");
+  expect(wrapper.text()).toContain("code source");
+});
